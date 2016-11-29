433850b3392e5682e79d86aa47f6f71c19ef068e
Sudarsana Nagineni <sudarsana.nagineni@intel.com>
2016-11-29 20:01:58 +0200

    ocfHandler: Update implementation to latest OCF JS spec
    Update the ocfHandler implementation to the latest OCF
    JS spec.
    
    Signed-off-by: Sudarsana Nagineni <sudarsana.nagineni@intel.com>

--- handlers/ocfHandler.js
+++ handlers/ocfHandler.js
@@ -1,8 +1,8 @@
 var OIC = require('../oic/oic');
-var DEV = require('iotivity-node')("client");
+var DEV = require('iotivity-node').client;
 
 const RESOURCE_FOUND_EVENT = "resourcefound";
-const RESOURCE_CHANGE_EVENT = "change";
+const RESOURCE_UPDATE_EVENT = "update";
 const RESOURCE_DELETE_EVENT = "delete";
 const DEVICE_FOUND_EVENT = "devicefound";
 const PLATFORM_FOUND_EVENT = "platformfound";
@@ -20,15 +20,6 @@ const notFoundStatusCode = 404; // Not found
 var discoveredResources = [];
 var discoveredDevices = [];
 var discoveredPlatforms = [];
-
-// Turn on global presence listening
-DEV.subscribe().then(
-    function() {
-        console.log("Subscribed for the presence notifications.");
-    },
-    function(error) {
-        console.log("device.subscribe() failed with: ", error);
-    });
 
 var routes = function(req, res) {
 
@@ -49,18 +40,18 @@ var routes = function(req, res) {
         }
     }
 
-    function onResourceFound(event) {
-        var resource = OIC.parseRes(event);
+    function onResourceFound(resourceInfo) {
+        var resource = OIC.parseRes(resourceInfo);
         discoveredResources.push(resource);
     }
 
-    function onDeviceFound(event) {
-        var device = OIC.parseDevice(event);
+    function onDeviceFound(deviceInfo) {
+        var device = OIC.parseDevice(deviceInfo);
         discoveredDevices.push(device);
     }
 
-    function onPlatformFound(event) {
-        var platform = OIC.parsePlatform(event);
+    function onPlatformFound(platformInfo) {
+        var platform = OIC.parsePlatform(platformInfo);
         discoveredPlatforms.push(platform);
     }
 
@@ -72,7 +63,7 @@ var routes = function(req, res) {
     function discoverResources(req, res) {
         console.log("discoverResources");
         res.setTimeout(timeoutValue, function() {
-            DEV.removeEventListener(RESOURCE_FOUND_EVENT, onResourceFound);
+            DEV.removeListener(RESOURCE_FOUND_EVENT, onResourceFound);
             res.writeHead(okStatusCode, 'Content-Type', 'application/json');
             res.end(JSON.stringify(discoveredResources));
         });
@@ -80,7 +71,7 @@ var routes = function(req, res) {
         console.log("%s %s", req.method, req.url);
 
         discoveredResources.length = 0;
-        DEV.addEventListener(RESOURCE_FOUND_EVENT, onResourceFound);
+        DEV.on(RESOURCE_FOUND_EVENT, onResourceFound);
 
         console.log("Discovering resources for %d seconds.", timeoutValue/1000);
         DEV.findResources().then(function() {
@@ -95,7 +86,7 @@ var routes = function(req, res) {
 
     function discoverDevices(req, res) {
         res.setTimeout(timeoutValue, function() {
-            DEV.removeEventListener(DEVICE_FOUND_EVENT, onDeviceFound);
+            DEV.removeListener(DEVICE_FOUND_EVENT, onDeviceFound);
             res.writeHead(okStatusCode, {'Content-Type': 'application/json'});
             res.end(JSON.stringify(discoveredDevices));
         });
@@ -103,7 +94,7 @@ var routes = function(req, res) {
         console.log("%s %s", req.method, req.url);
 
         discoveredDevices.length = 0;
-        DEV.addEventListener(DEVICE_FOUND_EVENT, onDeviceFound);
+        DEV.on(DEVICE_FOUND_EVENT, onDeviceFound);
 
         console.log("Discovering devices for %d seconds.", timeoutValue/1000);
         DEV.findDevices().then(function() {
@@ -118,7 +109,7 @@ var routes = function(req, res) {
 
     function discoverPlatforms(req, res) {
         res.setTimeout(timeoutValue, function() {
-            DEV.removeEventListener(PLATFORM_FOUND_EVENT, onPlatformFound);
+            DEV.removeListener(PLATFORM_FOUND_EVENT, onPlatformFound);
             res.writeHead(okStatusCode, {'Content-Type': 'application/json'});
             res.end(JSON.stringify(discoveredPlatforms));
         });
@@ -126,7 +117,7 @@ var routes = function(req, res) {
         console.log("%s %s", req.method, req.url);
 
         discoveredPlatforms.length = 0;
-        DEV.addEventListener(PLATFORM_FOUND_EVENT, onPlatformFound);
+        DEV.on(PLATFORM_FOUND_EVENT, onPlatformFound);
 
         console.log("Discovering platforms for %d seconds.", timeoutValue/1000);
         DEV.findPlatforms().then(function() {
@@ -147,38 +138,43 @@ var routes = function(req, res) {
         }
         console.log("%s %s (fd: %d)", req.method, req.url, req.socket._handle.fd);
 
-        function observer(event) {
+        function observer(resource) {
             var fd = (res.socket._handle == null) ? -1 : res.socket._handle.fd;
             console.log("obs: %d, fin: %s, di: %s, fd: %d",req.query.obs, res.finished, req.query.di, fd);
             if (req.query.obs == true && res.finished == false) {
-                var json = OIC.parseResource(event.resource);
+                var json = OIC.parseResource(resource);
                 res.write(json);
             } else {
-                event.resource.removeEventListener(RESOURCE_CHANGE_EVENT, observer);
-                event.resource.removeEventListener(RESOURCE_DELETE_EVENT, deleteHandler);
+                resource.removeListener(RESOURCE_UPDATE_EVENT, observer);
+                // FIXME: To be uncommented when iotivity-node issue#92 is fixed.
+                //resource.removeListener(RESOURCE_DELETE_EVENT, deleteHandler);
             }
         }
 
-        function deleteHandler(event) {
+        function deleteHandler(resource) {
             console.log("Resource %s has been deleted", req.url);
             if (req.query.obs == true && res.finished == false) {
                 res.end();
             }
         }
 
-        DEV.retrieve({deviceId: req.query.di, path: req.path}, req.query).then(
+        DEV.retrieve({deviceId: req.query.di, resourcePath: req.path}, req.query).then(
             function(resource) {
                 if (req.query.obs != "undefined" && req.query.obs == true) {
                     req.on('close', function() {
                         console.log("Client: close");
-                        resource.removeEventListener(RESOURCE_CHANGE_EVENT, observer);
-                        resource.removeEventListener(RESOURCE_DELETE_EVENT, deleteHandler);
+                        if (resource.observable)
+                            resource.removeListener(RESOURCE_UPDATE_EVENT, observer);
+                        // FIXME: To be uncommented when iotivity-node issue#92 is fixed.
+                        //resource.removeListener(RESOURCE_DELETE_EVENT, deleteHandler);
                         req.query.obs = false;
                     });
                     res.writeHead(okStatusCode, {'Content-Type':'application/json'});
                     req.setTimeout(socketTimeoutValue);
-                    resource.addEventListener(RESOURCE_CHANGE_EVENT, observer);
-                    resource.addEventListener(RESOURCE_DELETE_EVENT, deleteHandler);
+                    if (resource.observable)
+                        resource.on(RESOURCE_UPDATE_EVENT, observer);
+                    // FIXME: To be uncommented when iotivity-node issue#92 is fixed.
+                    // resource.on(RESOURCE_DELETE_EVENT, deleteHandler);
                 } else {
                     var json = OIC.parseResource(resource);
                     res.writeHead(okStatusCode, {'Content-Type':'application/json'});
@@ -210,7 +206,8 @@ var routes = function(req, res) {
         }).on('end', function() {
             body = Buffer.concat(body).toString();
             var resource = {
-                id: {deviceId: req.query.di, path: req.path},
+                deviceId: req.query.di,
+                resourcePath: req.path,
                 properties: JSON.parse(body)
             };
             console.log("PUT %s: %s", req.originalUrl, JSON.stringify(resource));
